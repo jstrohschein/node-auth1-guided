4cc4c5c310ea4ab5e0a34461a60de6a98cdb3f27
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-09 19:47:33 +0100

    wip

--- users/users-router.js
+++ users/users-router.js
@@ -4,7 +4,12 @@ const Users = require("./users-model.js");
 
 // put this middleware in a centralized location
 function secure(req, res, next) {
-  next()
+  // check if there is a user in the session
+  if (req.session && req.session.user) {
+    next()
+  } else {
+    res.status(401).json({ message: 'Unauthorized!' })
+  }
 }
 
 router.get("/", secure, (req, res) => {
