2068876048a4efe17dd00e222bbce497fcbee840
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-09 18:27:53 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.post('/auth/register', async (req, res) => {
   try {
     const { username, password } = req.body
     // do the hash, add the hash to the db
-    const hash = bcrypt.hashSync
+    const hash = bcrypt.hashSync(password, 10) // 2 ^ 10 rounds of hashing
   } catch (err) {
     // res.status(500).json({ message: 'Something went terrible' }) // PRODUCTION
     res.status(500).json({ message: err.message })
